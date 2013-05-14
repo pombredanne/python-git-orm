d8540110657350e0324a511d3939156f8d9f2585
Martin Natano <natano@natano.net>
2013-05-15 01:11:12 +0200

    remove init_repo

--- git_orm/__init__.py
+++ git_orm/__init__.py
@@ -43,28 +43,3 @@ def get_config(name):
     if repo is None:
         raise GitError('no repository found')
     return repo.config[name]
-
-
-def is_repo_initialized():
-    repo = _repository
-    if repo is None:
-        return False
-    ref = 'refs/heads/{}'.format(_branch)
-    try:
-        repo.lookup_reference(ref)
-    except KeyError:
-        return False
-    return True
-
-
-def init_repo():
-    import pkg_resources
-    from git_orm import transaction
-
-    if is_repo_initialized():
-        raise GitError('repository is already initialized')
-
-    with transaction.wrap() as trans:
-        tigetrc = pkg_resources.resource_string('tiget', 'data/tigetrc')
-        trans.set_blob(['config', 'tigetrc'], tigetrc)
-        trans.add_message('Initialize Repository')
